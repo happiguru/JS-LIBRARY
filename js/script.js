dddb0345cf4e06e8b9d79a4486ea8fbb2c2f14d3
victorG <peter200970@hotmail.com>
2020-11-25 11:43:42 -0500

    Add book constructor and addbooktolibrary function

--- js/script.js
+++ js/script.js
@@ -2,11 +2,27 @@
 
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
-
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const title = document.getElementById('booktitle').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const status = document.getElementById('status').value;
+
+  let book = new Book(title, author, pages, readStatus);
+
+  myLibrary.push(book);
 }
+
+
+const button = document.querySelector('btn');
+
+button.addEventListener('click', addBookToLibrary);
+
+
