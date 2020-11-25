b87df4f2d253ef2b625ddb04a39f73aac8fef5f7
hguruman <hguruman@gmail.com>
2020-11-25 22:30:02 +0400

    Fix bugs to get a working code

--- js/script.js
+++ js/script.js
@@ -3,41 +3,24 @@
 const bookList = document.getElementById('book-display');
 const form = document.getElementById('book-form');
 const showFormBtn = document.querySelector('.show-form')
-const button = document.querySelector('#submit');
+const submit = document.querySelector('#submit');
 const myLibrary = JSON.parse(localStorage.getItem('myLibrary')) || [];
 
-function Book(title, author, pages, readStatus) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.readStatus = readStatus;
+  this.read = read;
 }
 
 const saveLocalStorage = () => {
     localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-}
-
-function addBookToLibrary() {
-  const title = document.getElementById('booktitle').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const status = document.getElementById('status').value;
-
-  const book = new Book(title, author, pages, readStatus);
-
-  myLibrary.push(book);
-  displayLibrary(myLibrary);
-  saveLocalStorage();
-  formData.reset();
 };
 
-
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    addBookToLibrary();
-    form.style.display = 'none';
-});
-
+const changeReadStatus = (status) => {
+    const result = status ? 'You have already read this book!' : "You haven't read this book yet!";
+    return result;
+};
 
 const displayLibrary = (array) => {
     while(bookList.firstChild) {
@@ -55,27 +38,85 @@ const displayLibrary = (array) => {
             saveLocalStorage();
         });
         return readBtn;
+    };
+
+    const createDeleteBtn = (book) => {
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+
+        deleteBtn.addEventListener('click', () => {
+            const index = myLibrary.findIndex(item.title === book.title);
+            myLibrary.splice(index, 1);
+            displayLibrary(myLibrary);
+            saveLocalStorage();
+        });
+        return deleteBtn;
+    };
+
+    for(let i =0; i< array.length; i++){
+        const bookSection = document.createElement('li');
+        const title = document.createElement('h3');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+        const read = document.createElement('p');
+        read.classList.add('read-status');
+
+        title.textContent = array[i].title;
+        author.textContent = array[i].author;
+        pages.textContent = `${array[i].pages} pages`;
+        read.textContent = changeReadStatus(array[i].read);
+        bookSection.appendChild(title);
+        bookSection.appendChild(author);
+        bookSection.appendChild(pages);
+        bookSection.appendChild(read);
+        bookList.appendChild(bookSection);
+
+        const readBtn = createReadBtn(array[i]);
+        const deleteBtn = createDeleteBtn(array[i]);
+
+        readBtn.classList.add('btn', 'btn-primary', 'btn-style');
+        deleteBtn.classList.add('btn', 'btn-primary', 'btn-style');
+
+        bookSection.appendChild(readBtn);
+        bookSection.appendChild(deleteBtn);
     }
-}
+};
 
-for(let i=0; i<Array.length; i++){
-    const bookSection = document.createElement('li');
-    const title = document.createElement('h1');
-    const author = document.createElement('p');
-    const pages = document.createElement('p');
-    const read = document.createElement('p');
-    read.createList.add('status');
+const addBookToLibrary = () =>  {
+    const title = document.getElementById('booktitle').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('status').checked;
+  
+    const book = new Book(title, author, pages, read);
+  
+    myLibrary.push(book);
+    displayLibrary(myLibrary);
+    saveLocalStorage();
+    form.reset();
+  };
+  
+  
+  submit.addEventListener('click', (e) => {
+      e.preventDefault();
+      addBookToLibrary();
+      form.style.display = 'none';
+  });
 
-    title.textContent = array[i].title;
-    author.textContent = array[i].author;
-    pages.textContent = `${array[i].pages} pages`;
-    read.textContent = changeReadStatus(array[i].read);
+const defaultBooks = () => {
+    const book1 = new Book('When Rain Cloud Gathers', 'Charles Dickens', 129, true);
+};
 
-    bookSection.appendChild(title);
-    bookSection.appendChild(author);
-    bookSection.appendChild(pages);
-    bookSection.appendChild(read);
-    bookList.appendChild(bookSection);
-}
+  const showForm = () => {
+      if(form.style.display === 'block') form.style.display = 'none';
+      else form.style.display = 'block';
+  };
 
-displayLibrary(myLibrary);
+  if(localStorage.getItem('myLibrary') === null){
+      defaultBooks();
+  }
+
+  displayLibrary(myLibrary);
+
+  showFormBtn.onclick = () => showForm();
+  
